09a960ea0418b51558c500b90295d901e4909511
github-action-benchmark <github@users.noreply.github.com>
2026-05-01 12:31:38 +0000

    add Zen Compiler Benchmarks (customSmallerIsBetter) benchmark result for 8373cefc3279adcaf8d0deaf7123652bbe5bb6a5

---
+++ dev/bench/data.js
@@ -0,0 +1,57 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1777638698149,
+  "repoUrl": "https://github.com/zenc-lang/zenc",
+  "entries": {
+    "Zen Compiler Benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "email": "zuhaitz.zechhub@gmail.com",
+            "name": "Zuhaitz-dev",
+            "username": "Zuhaitz-dev"
+          },
+          "committer": {
+            "email": "zuhaitz.zechhub@gmail.com",
+            "name": "Zuhaitz-dev",
+            "username": "Zuhaitz-dev"
+          },
+          "distinct": true,
+          "id": "8373cefc3279adcaf8d0deaf7123652bbe5bb6a5",
+          "message": "ci: harden benchmark workflow gh-pages check",
+          "timestamp": "2026-05-01T13:30:54+01:00",
+          "tree_id": "9b12aac5267b4ad5afef936938e62ebf4169acae",
+          "url": "https://github.com/zenc-lang/zenc/commit/8373cefc3279adcaf8d0deaf7123652bbe5bb6a5"
+        },
+        "date": 1777638697422,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Compiler (Full Suite Transpilation)",
+            "value": 13487,
+            "unit": "ms"
+          },
+          {
+            "name": "Compiler (Avg per file)",
+            "value": 38,
+            "unit": "ms"
+          },
+          {
+            "name": "Runtime (hello)",
+            "value": 0,
+            "unit": "ms"
+          },
+          {
+            "name": "Runtime (sha256)",
+            "value": 50,
+            "unit": "ms"
+          },
+          {
+            "name": "Runtime (sort_large)",
+            "value": 72,
+            "unit": "ms"
+          }
+        ]
+      }
+    ]
+  }
+}
