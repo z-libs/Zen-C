a8d0f51fb0281503195dc96685b44449b1c2f063
github-action-benchmark <github@users.noreply.github.com>
2026-05-01 18:27:17 +0000

    add Zen Compiler Benchmarks (customSmallerIsBetter) benchmark result for 9e855e8cd0d7182170212339cf33e8fd8d489d85

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1777638698149,
+  "lastUpdate": 1777660037605,
   "repoUrl": "https://github.com/zenc-lang/zenc",
   "entries": {
     "Zen Compiler Benchmarks": [
@@ -48,6 +48,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "Runtime (sort_large)",
             "value": 72,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "zuhaitz.zechhub@gmail.com",
+            "name": "Zuhaitz-dev",
+            "username": "Zuhaitz-dev"
+          },
+          "committer": {
+            "email": "zuhaitz.zechhub@gmail.com",
+            "name": "Zuhaitz-dev",
+            "username": "Zuhaitz-dev"
+          },
+          "distinct": true,
+          "id": "9e855e8cd0d7182170212339cf33e8fd8d489d85",
+          "message": "zenc: more robustness and fixed edge cases.",
+          "timestamp": "2026-05-01T19:26:34+01:00",
+          "tree_id": "7a8dfa5df11a9dfe7aafb86ca729ee582b237ede",
+          "url": "https://github.com/zenc-lang/zenc/commit/9e855e8cd0d7182170212339cf33e8fd8d489d85"
+        },
+        "date": 1777660037201,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Compiler (Full Suite Transpilation)",
+            "value": 12131,
+            "unit": "ms"
+          },
+          {
+            "name": "Compiler (Avg per file)",
+            "value": 34,
+            "unit": "ms"
+          },
+          {
+            "name": "Runtime (hello)",
+            "value": 0,
+            "unit": "ms"
+          },
+          {
+            "name": "Runtime (sha256)",
+            "value": 49,
+            "unit": "ms"
+          },
+          {
+            "name": "Runtime (sort_large)",
+            "value": 77,
             "unit": "ms"
           }
         ]
