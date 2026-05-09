f6a6353075e6c710efee2ff942998dd6ef17e8b2
Zuhaitz-dev <zuhaitz.zechhub@gmail.com>
2026-05-09 08:33:16 +0100

    zenc: test refactor.

---
+++ tests/compiler/lsp/test_runner.d
@@ -0,0 +1,17 @@
+tests/compiler/lsp/test_runner: src/lsp/cJSON.c src/lsp/../zprep.h \
+ src/lsp/../platform/lang.h src/lsp/../platform/os.h \
+ src/lsp/../platform/lang.h src/lsp/../platform/arch.h \
+ src/lsp/../utils/colors.h src/lsp/../utils/zvec.h \
+ src/lsp/../utils/zalloc.h src/lsp/../diagnostics/diagnostics.h \
+ src/zprep.h src/lsp/cJSON.h
+src/lsp/../zprep.h:
+src/lsp/../platform/lang.h:
+src/lsp/../platform/os.h:
+src/lsp/../platform/lang.h:
+src/lsp/../platform/arch.h:
+src/lsp/../utils/colors.h:
+src/lsp/../utils/zvec.h:
+src/lsp/../utils/zalloc.h:
+src/lsp/../diagnostics/diagnostics.h:
+src/zprep.h:
+src/lsp/cJSON.h:
